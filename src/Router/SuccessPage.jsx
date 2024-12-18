da06a90fae6fcca7ef93b60ae9f7a334606ce30d
Mostafa-Sukhnia <mustafa.2005.sukhnia@gmail.com>
2024-12-18 23:00:08 +0300

    dead2

--- src/Router/SuccessPage.jsx
+++ src/Router/SuccessPage.jsx
@@ -3,9 +3,11 @@ import { useNavigate } from "react-router-dom";
 
 const SuccessPage = () => {
   const navigate = useNavigate();
+
   useEffect(() => {
-    const refererr =document.referrer;
-    if (refererr != "https://buy.stripe.com/") {
+    const referer = document.referrer;
+
+    if (!referer.startsWith("https://buy.stripe.com/")) {
       navigate("/");
     }
   }, [navigate]);
