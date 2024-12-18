cadde3c1f88d8c269be2c7bdb47706296bc9782a
Mostafa-Sukhnia <mustafa.2005.sukhnia@gmail.com>
2024-12-18 22:51:03 +0300

    update

--- src/Router/SuccessPage.jsx
+++ src/Router/SuccessPage.jsx
@@ -1,19 +1,16 @@
-import React,{useEffect} from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SuccessPage = () => {
-
-const navigate = useNavigate();
-useEffect(()=>{
+  const navigate = useNavigate();
+  useEffect(() => {
     const refererr = document.referrer;
-    if(refererr !== 'https://buy.stripe.com'){
-        navigate('/');
+    if (refererr !== "https://buy.stripe.com/") {
+      navigate("/");
     }
-},[navigate])
+  }, [navigate]);
 
-  return (
-    <div>SuccessPage</div>
-  )
-}
+  return <div>SuccessPage</div>;
+};
 
-export default SuccessPage
+export default SuccessPage;
