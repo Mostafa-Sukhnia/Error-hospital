501e817c33fd79f7468cb7910eb413da0d93e078
Mostafa-Sukhnia <mustafa.2005.sukhnia@gmail.com>
2024-12-18 23:42:17 +0300

    update success

--- src/Router/SuccessPage.jsx
+++ src/Router/SuccessPage.jsx
@@ -1,10 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import app from '../config/firebase.js';
+import { db } from '../config/firebase.js';
+import { getAuth } from "firebase/auth";
+import { getDoc, doc } from "firebase/firestore";
 
 const SuccessPage = () => {
   const navigate = useNavigate();
-
+  const [userData, setUserData] = useState(null);
+  
   useEffect(() => {
+    const fetchUserData = async () => {
+      const auth = getAuth(app);
+      const userRef = doc(db, "users", auth.currentUser.uid); // تأكد من أن المستخدم مسجل الدخول
+      const userSnapshot = await getDoc(userRef);
+
+      if (userSnapshot.exists()) {
+        setUserData(userSnapshot.data()); // حفظ البيانات في الحالة
+      } else {
+        console.log("No such document!");
+      }
+    };
+
+    fetchUserData();
+
     const referer = document.referrer;
     console.log("Referrer:", referer);
     if (referer.startsWith("https://buy.stripe.com/")) {
@@ -14,7 +33,17 @@ const SuccessPage = () => {
     }
   }, [navigate]);
 
-  return <div>SuccessPage</div>;
+  return (
+    <div>
+      SuccessPage
+      {userData && (
+        <div>
+          <h3>User Data:</h3>
+          <pre>{JSON.stringify(userData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default SuccessPage;
