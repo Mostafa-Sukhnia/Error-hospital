d60a72b3c0267ec7f79257e6b1b8b38c347bd52f
Mostafa-Sukhnia <mustafa.2005.sukhnia@gmail.com>
2024-12-19 00:23:11 +0300

    69

--- src/Router/SuccessPage.jsx
+++ src/Router/SuccessPage.jsx
@@ -1,48 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { db } from "../config/firebase.js";
-import { getAuth } from "firebase/auth";
-import { getDoc, doc } from "firebase/firestore";
 
 const SuccessPage = () => {
   const navigate = useNavigate();
-  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      const auth = getAuth();
-      const user = auth.currentUser;
-
-      if (user) {
-        const userRef = doc(db, "users", user.uid);
-        const userSnapshot = await getDoc(userRef);
-        console.log(userSnapshot);
-        if (userSnapshot.exists()) {
-          setUserData(userSnapshot.data());
-        } else {
-          console.log("No user data found");
-        }
-      }
-    };
-
-    fetchUserData();
     const referer = document.referrer;
+    console.log("Referrer:", referer);
     if (referer.startsWith("https://buy.stripe.com/")) {
       console.log("Redirected from Stripe");
+    } else {
+      navigate("/");
     }
   }, [navigate]);
 
-  return (
-    <div>
-      SuccessPage
-      {userData && (
-        <div>
-          <h3>User Data:</h3>
-          <pre>{JSON.stringify(userData, null, 2)}</pre>
-        </div>
-      )}
-    </div>
-  );
+  return <div>SuccessPage</div>;
 };
 
 export default SuccessPage;
