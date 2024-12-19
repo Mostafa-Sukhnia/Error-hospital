dc72a74922e9e4c760d9a64485177727de5edef3
Mostafa-Sukhnia <mustafa.2005.sukhnia@gmail.com>
2024-12-19 13:08:21 +0300

    done

--- src/Router/SuccessPage.jsx
+++ src/Router/SuccessPage.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import {db} from '../config/firebase';
+import app from '../config/firebase.js';
+import {doc,getDoc} from 'firebase/firestore';
+import {getAuth} from 'firebase/auth'
 const SuccessPage = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    const auth = getAuth(app);
     const referer = document.referrer;
     console.log("Referrer:", referer);
     if (referer.startsWith("https://buy.stripe.com/")) {
       console.log("Redirected from Stripe");
+      if(auth){
+        const userRef = getDoc(doc(db,'users',auth.currentUser.uid));
+        const user = userRef.data();
+        console.log(user);
+      }
     } else {
       navigate("/");
     }
